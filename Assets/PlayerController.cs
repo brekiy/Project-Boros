4af83a5b55afca7edc2a955c5deca3cb83b8648d
xhuy <xhuy@umich.edu>
2018-01-26 21:34:42 -0500

    first commit extra clean

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+        var y = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        var cy = Input.GetAxis("CamVertical") * Time.deltaTime * 150.0f;
+
+        transform.Translate(x, 0, y);
+        transform.Rotate(0, cy, 0);
+    }
+}
