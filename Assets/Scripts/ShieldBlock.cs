1d8e1db5d7698a21cfd367c50a03d085e8879fbd
Joshua Rickard-Lukasavitz <jjmmff0@gmail.com>
2018-01-28 09:20:25 -0500

    dfsadfsjk;l:

---
+++ Assets/Scripts/ShieldBlock.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldBlock : MonoBehaviour {
+
+    HealthScript health;
+
+    // Use this for initialization
+    void Awake () {
+        health.SetHealth(1000,1000);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (health.GetHealth() != 1000)
+        {
+            transform.parent.parent.parent.parent.parent.parent.parent.parent.GetComponent<HealthScript>().DoDamage(1000 - health.GetHealth(), health.invulFrames);
+            health.SetHealth(1000,1000);
+        }
+
+    }
+}
